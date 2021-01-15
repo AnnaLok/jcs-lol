614bce92fabe1fa9cc18a7f44d814ae5759563a2
AnnaLok <a6lok@uwaterloo.ca>
2021-01-15 05:40:07 -0500

    Updates

---
+++ precache-manifest.ff23132d0e13479909ac33168ba6f27b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50476e0e3998159926b6acf1dc566a05",
+    "url": "/jcs-lol/index.html"
+  },
+  {
+    "revision": "aa49eed5344d544fb17b",
+    "url": "/jcs-lol/static/css/main.c5a3ee5c.chunk.css"
+  },
+  {
+    "revision": "68d427913e0d1842afa1",
+    "url": "/jcs-lol/static/js/2.ef7587c2.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/jcs-lol/static/js/2.ef7587c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa49eed5344d544fb17b",
+    "url": "/jcs-lol/static/js/main.982f97f0.chunk.js"
+  },
+  {
+    "revision": "e1c39b85dc6a26fb015e",
+    "url": "/jcs-lol/static/js/runtime-main.1866b74d.js"
+  },
+  {
+    "revision": "18b27feab7474eb7a52d795cfe4ab699",
+    "url": "/jcs-lol/static/media/home_page_bkg.18b27fea.jpg"
+  },
+  {
+    "revision": "2fa3cb01455cc657f775f2d82f274708",
+    "url": "/jcs-lol/static/media/logo.2fa3cb01.png"
+  }
+]);
